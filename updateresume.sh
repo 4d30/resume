add9c00c7e3539f86417d54b46a845e862e3ae01
Joe Mifsud <45021583+4d30@users.noreply.github.com>
2021-06-23 14:11:58 +0000

    fixed string-sub

--- updateresume.sh
+++ updateresume.sh
@@ -9,21 +9,21 @@ FILEPATH=${COMPANY_DIR}/${BASE_FILENAME}_resume
 LETTERPATH=${COMPANY_DIR}/${BASE_FILENAME}_letter
 
 
-printf "YOURCOMPANY THEROLE WHATIAM DOTHETHING"
+printf "YOURCOMPANY THEROLE WHATIHAVE DOTHETHING"
 #printf "%s" $1
 if [ ! -d ./$1 ]; then
 	{ mkdir ${COMPANY_DIR}; }
 fi
 
 sed -e "s/YOURCOMPANY/$(printf %s $1)/g" resume_GENERIC.tex |\
-sed -e "s/WHATIAM/$(printf %s $3)/g" |\
+sed -e "s/WHATIHAVE/$(printf %s $3)/g" |\
 sed -e "s/DOTHETHING/$(printf %s $4)/g" |\
 sed -e "s/THEROLE/$(printf %s $2)/g" >  ${FILEPATH}.tex
 xelatex -output-directory=${COMPANY_DIR} ${FILEPATH}.tex
 
 
 sed -e "s/YOURCOMPANY/$(printf %s $1)/g" letter_GENERIC.tex |\
-sed -e "s/WHATIAM/$(printf %s $3)/g" |\
+sed -e "s/WHATIHAVE/$(printf %s $3)/g" |\
 sed -e "s/DOTHETHING/$(printf %s $4)/g" |\
 sed -e "s/THEROLE/$(printf %s $2)/g" >  ${LETTERPATH}.tex
 xelatex -output-directory=${COMPANY_DIR} ${LETTERPATH}.tex
@@ -31,7 +31,7 @@ xelatex -output-directory=${COMPANY_DIR} ${LETTERPATH}.tex
 rm ${COMPANY_DIR}/*.log
 rm ${COMPANY_DIR}/*.aux
 
-./strip_tex.sh ${LETTERPATH}.tex | fmt > ${LETTERPATH}.txt
+./strip_letter.sh ${LETTERPATH}.tex | fmt > ${LETTERPATH}.txt
 
 sed -n '25,$p' ${LETTERPATH}.txt
 
